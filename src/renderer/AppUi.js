759854fb18c7249b6eaeded80ed6a9ec312b170d
mahongquan <mahongquan@sina.com>
2018-05-20 13:13:29 +0800

    add file

---
+++ src/renderer/AppUi.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import QueryResultTable from './components/QueryResultTable.jsx'
+export default class Root extends Component<Props> {
+  render() {
+    return (
+      <QueryResultTable />
+    );
+  }
+}
+
